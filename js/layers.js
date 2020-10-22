726dcb6dc27caa6888f1250ea7442006a00a8cc6
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-22 21:58:06 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,16 +1,16 @@
-addLayer("apt", {
-        name: "anti-planck time", // This is optional, only used in a few places, If absent it just uses the layer id.
-        symbol: "APT", // This appears on the layer's node. Default is the id with the first letter capitalized
+addLayer("p", {
+        name: "prestige", // This is optional, only used in a few places, If absent it just uses the layer id.
+        symbol: "P", // This appears on the layer's node. Default is the id with the first letter capitalized
         position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
         startData() { return {
             unlocked: true,
 			points: new Decimal(0),
         }},
-        color: "#FFFFFF",
+        color: "#4BDC13",
         requires: new Decimal(10), // Can be a function that takes requirement increases into account
-        resource: "plotinum points", // Name of prestige currency
-        baseResource: "plotinum", // Name of resource prestige is based on
-        baseAmount() {return player.plotinum}, // Get the current amount of baseResource
+        resource: "prestige points", // Name of prestige currency
+        baseResource: "points", // Name of resource prestige is based on
+        baseAmount() {return player.points}, // Get the current amount of baseResource
         type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
         exponent: 0.5, // Prestige currency exponent
         gainMult() { // Calculate the multiplier for main currency from bonuses
@@ -22,7 +22,7 @@ addLayer("apt", {
         },
         row: 0, // Row the layer is in on the tree (0 is the first row)
         hotkeys: [
-            {key: "a", description: "Reset for anti-planck times", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
+            {key: "p", description: "Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
         ],
         layerShown(){return true},
 })
