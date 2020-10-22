4120bba1d1aca2d6876ab8c22a3fd398257ce8aa
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-22 21:55:54 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -10,7 +10,7 @@ addLayer("apt", {
         requires: new Decimal(10), // Can be a function that takes requirement increases into account
         resource: "plotinum", // Name of prestige currency
         baseResource: "plotinum", // Name of resource prestige is based on
-        baseAmount() {return player.points}, // Get the current amount of baseResource
+        baseAmount() {return player.plotinum}, // Get the current amount of baseResource
         type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
         exponent: 0.5, // Prestige currency exponent
         gainMult() { // Calculate the multiplier for main currency from bonuses
