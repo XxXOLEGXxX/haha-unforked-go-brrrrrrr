3e7eef762e65c6e719fc09b7f451d786a15fd724
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-23 22:21:19 +0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -35,7 +35,6 @@ function getPointGen() {
 
 	let gain = new Decimal(0.01666666666666666666666666666667)
         if (hasUpgrade("s", 11)) gain = gain.add(new Decimal(0.98333333333333333333333333333333));
-        gain = gain.mul(layers.k.effect().pointGainBaseIncrease);
 
 	return gain
 }
