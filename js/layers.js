c674672e474873067c1b887a21fa8dde2284638d
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-23 16:01:06 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
- addLayer("p": {
+ addLayer("p", {
         startData() { return {                  // startData is a function that returns default data for a layer. 
             unlocked: true,                    // You can add more variables here to add them to your layer.
             points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
