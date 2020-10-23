4dea4e0cafebe1b8362bfc0bc6a2ba6ea3409855
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-23 15:56:12 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,57 +1,28 @@
-addLayer("apt", {
-        name: "anti-planck time", // This is optional, only used in a few places, If absent it just uses the layer id.
-        symbol: "APT", // This appears on the layer's node. Default is the id with the first letter capitalized
-        position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-        startData() { return {
-            unlocked: true,
-			points: new Decimal(0),
+ p: {
+        startData() { return {                  // startData is a function that returns default data for a layer. 
+            unlocked: false,                    // You can add more variables here to add them to your layer.
+            points: new Decimal(0),             // "points" is the internal name for the main resource of the layer.
         }},
-        color: "#424242",
-        requires: new Decimal(10), // Can be a function that takes requirement increases into account
-        resource: "anti-planck times", // Name of prestige currency
-        baseResource: "mysterious shits", // Name of resource prestige is based on
-        baseAmount() {return player.points}, // Get the current amount of baseResource
-        type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-        exponent: 0.5, // Prestige currency exponent
-        gainMult() { // Calculate the multiplier for main currency from bonuses
-            mult = new Decimal(1)
-            return mult
-        },
-        gainExp() { // Calculate the exponent on main currency from bonuses
-            return new Decimal(1)
-        },
-        row: 0, // Row the layer is in on the tree (0 is the first row)
-        hotkeys: [
-            {key: "t", description: "Reset for anti-planck times", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-        ],
-        layerShown(){return true},
-})
 
-addLayer("apl", {
-        name: "anti-planck length", // This is optional, only used in a few places, If absent it just uses the layer id.
-        symbol: "APL", // This appears on the layer's node. Default is the id with the first letter capitalized
-        position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
-        startData() { return {
-            unlocked: true,
-			points: new Decimal(0),
-        }},
-        color: "#F4F2F0",
-        requires: new Decimal(10), // Can be a function that takes requirement increases into account
-        resource: "anti-planck lengths", // Name of prestige currency
-        baseResource: "mysterious shits 2", // Name of resource prestige is based on
-        baseAmount() {return player.points}, // Get the current amount of baseResource
-        type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
-        exponent: 0.5, // Prestige currency exponent
-        gainMult() { // Calculate the multiplier for main currency from bonuses
-            mult = new Decimal(1)
-            return mult
+        color: "#FE0102",                       // The color for this layer, which affects many elements
+        resource: "prestige points",            // The name of this layer's main prestige resource
+        row: 0,                                 // The row this layer is on (0 is the first row)
+
+        baseResource: "points",                 // The name of the resource your prestige gain is based on
+        baseAmount() {return player.points},    // A function to return the current value of that resource
+
+        requires: new Decimal(200),            // The amount of the base needed to  gain 1 of the prestige currency.
+                                                // Also the amount required to unlock the layer.
+        
+        type: "normal",                         // Determines the formula used for calculating prestige currency.
+        exponent: 0.5,                          // "normal" prestige gain is (currency^exponent)
+
+        gainMult() {                            // Returns your multiplier to your gain of the prestige resource
+            return new Decimal(1)               // Factor in any bonuses multiplying gain here
         },
-        gainExp() { // Calculate the exponent on main currency from bonuses
+        gainExp() {                             // Returns your exponent to your gain of the prestige resource
             return new Decimal(1)
         },
-        row: 0, // Row the layer is in on the tree (0 is the first row)
-        hotkeys: [
-            {key: "l", description: "Reset for anti-planck lengths", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-        ],
-        layerShown(){return true},
-})
+
+        layerShown() {return true},             // Returns a bool for if this layer's node should be visible in the tree.
+    },
