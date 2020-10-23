3da63960ac1ab78677e59e98d27851098d3d3981
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-23 15:52:32 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -25,15 +25,15 @@ addLayer("apt", {
             {key: "t", description: "Reset for anti-planck times", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
         ],
         layerShown(){return true},
-   upgrades: {
-        rows: 1,
-        cols: 1,
+  upgrades: {
+        rows: 1
+        cols: 1
         11: {
-            title: "Time Acceleration.",
-            description: "Increases your anti-planck time generation exponentially, based on your anti-planck times.",
-            effect(): mult = new Decimal(1^1.1),
-        },
-    },
+            description: "Blah",
+            more features
+        }
+        etc
+    }
 })
 
 addLayer("apl", {
