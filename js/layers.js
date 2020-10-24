a7f6cfdb98e478a50545ab4378ccb0b41dfb54ed
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-24 09:56:52 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -2,9 +2,7 @@
         startData() { return {                  // startData is a function that returns default data for a layer. 
             unlocked: true,                    // You can add more variables here to add them to your layer.
             points: new Decimal(1),             // "points" is the internal name for the main resource of the layer.
-            update(diff) {
-            generatePoints("s", diff)
-            },
+            update(diff)
         }},
 
         name: "shenanigans",
@@ -117,6 +115,9 @@
             cost: new Decimal(1),
             unlocked() { 
                 return hasUpgrade([this.layer], 31);
+            effect() {
+            addPoints("s", diff)
+            },
             },
         },
     },
