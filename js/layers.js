db0dcedbec6ef3cb18ea8f4f0027a8f8ea64d591
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-24 09:23:30 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -4,8 +4,7 @@
             points: new Decimal(1),             // "points" is the internal name for the main resource of the layer.
             update(diff) {
 		 if (hasUpgrade("s", 32)) {
-                 let freefarm = tmp.resetGain["s"].mul(0.1).mul(1000)
-		 addPoints("s", freefarm)
+                 generatePoints("s", diff)
             }
         },
         }},
