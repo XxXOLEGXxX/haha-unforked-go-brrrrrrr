986191dd38b9c992b5d351d85ca183fa5f78c41e
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-25 14:30:53 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -158,13 +158,14 @@
             player[this.layer].buyables[this.id] = player[this.layer].buyables[this.id].add(1)
 	    },
             effect() {
-            let ret = player[this.layer].buyables[this.id].div(100).add(1);
+            let eff = {}
+            let eff.first = player[this.layer].buyables[this.id].div(100).add(1);
             return ret
 	    },
             display() { // Everything else displayed in the buyable button after the title
                 let data = tmp[this.layer].buyables[this.id]
                 return "Amount: " + player[this.layer].buyables[this.id] + "\n\
-                Knowing that you're being forced to grind the plots, you're getting more bored and it somehow magically boosts your plot gain by " + format(data.ret) + " times."
+                Knowing that you're being forced to grind the plots, you're getting more bored and it somehow magically boosts your plot gain by " + format(data.effect.first) + " times."
 	    },
 	},
     },
