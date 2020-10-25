45d06b06fce6f210f354407c93760db50221f69e
XxXOLEGXxX <olegmelnikundertalewarface@mail.ru>
2020-10-25 15:05:00 +0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -157,14 +157,15 @@
             buy() {
             player[this.layer].buyables[this.id] = player[this.layer].buyables[this.id].add(1)
 	    },
-            effect(x) {
-	        let eff = x.mul(0.01).add(1)
+            effect() {
+	        let eff = player[this.layer].buyables[this.id].mul(0.01).add(1)
 	        return eff;
 	    },
 	    display() { // Everything else displayed in the buyable button after the title
-		    return "Level " + player[this.layer].buyables[this.id] + "\n\
+                    let ret = {}
+		    return "Amount " + player[this.layer].buyables[this.id] + "\n\
 		    Cost: Boosts the finished work's effect.\n\
-		    Knowing that you're being forced to grind the plots, you're getting more bored and it somehow magically boosts your plot gain by " + buyableEffect([this.layer], 11) + "times."
+		    Knowing that you're being forced to grind the plots, you're getting more bored and it somehow magically boosts your plot gain by " + buyableEffect([this.layer], [this.id]) + "times."
 		},
 	},
     },
